43e74d065062bdba2b5da4fcdacecc65e0ec92cf
Fashina Segun <fashina.segun25@gmail.com>
2023-01-11 16:32:44 -0800

    configuration_management

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,5 @@
-# Using Puppet, install puppet-lint
-package { 'puppet-lint':
-  ensure          =>  'installed',
-  install_options =>  ['-v', '2.1.1'],
+# Using Puppet, install flask from pip3
+package { 'flask':
+  ensure   => '2.1.0',
+  provider => 'pip3',
 }
